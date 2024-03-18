eaff7fa0f446909cb342cd9b671996e9beade07e
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-03-18 20:10:54 +0000

    Completed views to be flexible with currency types and current real-time prices

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
+# app.rb
+
 require "sinatra"
 require "sinatra/reloader"
 require "http"
+require "json"
 
 # ----------------------------------------- #
 
 get("/") do
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
 
   # use HTTP.get to retrieve the API information
   raw_data = HTTP.get(api_url)
@@ -17,7 +20,7 @@ get("/") do
   parsed_data = JSON.parse(raw_data_string)
 
   # get the symbols from the JSON
-  # @symbols = parsed_data ...
+  @symbols = parsed_data.fetch("currencies")
 
   # render a view template where I show the symbols
   erb(:homepage)
@@ -28,7 +31,19 @@ end
 get("/:from_currency") do
   @original_currency = params.fetch("from_currency")
 
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @symbols = parsed_data.fetch("currencies")
 
   # some more code to parse the URL and render a view template
   erb(:from)
@@ -40,9 +55,13 @@ get("/:from_currency/:to_currency") do
   @original_currency = params.fetch("from_currency")
   @destination_currency = params.fetch("to_currency")
 
-  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  api_url = "http://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
 
-  @amount = HTTP.get(api_url)
+  all = HTTP.get(api_url)
+
+  parsed_data = JSON.parse(all)
+
+  @amount = parsed_data.fetch("result")
 
   erb(:results)
 end
